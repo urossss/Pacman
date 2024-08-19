da7da9f37a983ea942a9e94b37b8701e89951c28
Uros Stojiljkovic <urosss007@gmail.com>
2024-08-19 18:38:07 +0200

    Added loading screen animation

--- src/pacman/states/LoadingState.java
+++ src/pacman/states/LoadingState.java
@@ -6,23 +6,32 @@ import java.awt.*;
 
 public class LoadingState extends State {
 
+	private long previousTime;
+	private long timer;
+
 	public LoadingState(Handler handler) {
 		super(handler, false, false, false);
 	}
 
 	@Override
 	public void startImpl() {
-
+		this.timer = 0;
+		this.previousTime = System.currentTimeMillis();
 	}
 
 	@Override
 	public void updateImpl() {
-
+		long currentTime = System.currentTimeMillis();
+		this.timer += currentTime - this.previousTime;
+		this.previousTime = currentTime;
 	}
 
 	@Override
 	public void renderImpl(Graphics g) {
-		g.setColor(Color.red);
-		g.fillRect(0, 0, 100, 100);
+		g.setColor(Color.white);
+		int dotCount = ((int) this.timer / 350) % 4;
+		for (int i = 0; i < dotCount; i++) {
+			g.fillRect(220 + i * 10, 280, 4, 4);
+		}
 	}
 }
